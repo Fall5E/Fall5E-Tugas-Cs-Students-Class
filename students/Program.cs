d856e60f96ddaa48641d6c10589bc203bd018b75
MUHAMMAD NAUFAL NAWARUDDIN SYAH <m.naufal664@students.amikom.ac.id>
2026-04-18 02:22:27 +0700

    tugas student class c sharp

---
+++ students/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            students s1 = new students();
+            s1.name = "Andi";
+            s1.age = 20;
+            s1.studentId = "S001";
+            s1.score = 85;
+            s1.displayInfo();
+            s1.CheckStatus();
+            Console.WriteLine();
+
+            students s2 = new students();
+            s2.name = "Budi";
+            s2.age = 21;
+            s2.studentId = "S002";
+            s2.score = 60;
+
+            s2.displayInfo();
+            s2.CheckStatus();
+
+        }
+    }
+}
---
+++ students/students.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace students
+{
+    internal class students
+    {
+        public string name;
+        public int age;
+        public string studentId;
+        public double score;
+
+        public void displayInfo()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Student ID: " + studentId);
+            Console.WriteLine("Score: " + score);
+
+        }
+        public void CheckStatus()
+        {
+            if (score >= 75)
+            {
+                Console.WriteLine("Status: Passed");
+            }
+            else
+            {
+                Console.WriteLine("Status: Failed");
+            }
+        }
+    }
+}
